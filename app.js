d9efb315d0ec7e3c64c9f3140772ecf2a292af58
aniruddhabarua <aniruddhabarua15@gmail.com>
2026-02-24 00:33:09 +0600

    added available jobs tabs  toggle logic in js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ let countTotal = document.getElementById('count-total')
 let countInterview = document.getElementById('count-interview')
 let countRejected = document.getElementById('count-rejected')
 
+let jobButton = document.getElementById('job-btn')
+let interviewButton = document.getElementById('interview-btn')
+let rejectedButton = document.getElementById('rejected-btn')
+
 const jobCardsCount = document.getElementById('job-card-container')
 const mainContainer = document.querySelector('main')
 
@@ -14,3 +18,11 @@ function calculateCount(){
     countRejected.innerText = rejectedArray.length
 }
 calculateCount()
+
+function toggleTab(id){
+    let allButtons = document.querySelectorAll('.tab-btn')
+    for(let i = 0; i < allButtons.length; i++){
+        allButtons[i].classList.remove('active')
+    }
+    document.getElementById(id).classList.add('active')
+}
