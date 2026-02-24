f1ae7b8bdc80d0d4a4f9f554d197f6f9acdb0b31
aniruddhabarua <aniruddhabarua15@gmail.com>
2026-02-24 18:27:48 +0600

    added rejected tab logic (according to conceptual session but not working fully) and rejected count

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let countTotal = document.getElementById('count-total')
 let countInterview = document.getElementById('count-interview')
 let countRejected = document.getElementById('count-rejected')
+currentStatus = "job-btn"
 
 let jobButton = document.getElementById('job-btn')
 let interviewButton = document.getElementById('interview-btn')
@@ -26,14 +27,21 @@ function toggleTab(id){
         allButtons[i].classList.remove('active')
     }
     document.getElementById(id).classList.add('active')
+    currentStatus = id
 
     if (id == 'interview-btn'){
         jobCardsCount.classList.add('hidden')
         filteredSection.classList.remove('hidden')
+        interviewRender()
     }
     else if (id == 'job-btn'){
         jobCardsCount.classList.remove('hidden')
         filteredSection.classList.add('hidden')
+    }
+    else if (id == 'rejected-btn'){
+        jobCardsCount.classList.add('hidden')
+        filteredSection.classList.remove('hidden')
+        rejectedRender()
     }
 }
 
@@ -54,14 +62,65 @@ mainContainer.addEventListener('click', function(event){
         if (!interviewExist){
             interviewArray.push(cardInfo)
         }
+        rejectedArray = rejectedArray.filter(item => item.jobTitle != cardInfo.jobTitle)
+        if (currentStatus == "rejected-btn") {
+            rejectedRender();
+        }
         calculateCount()
-        interviewRender()
+    }
+    else if (event.target.classList.contains('job-btn-2')){
+        const parenNode = event.target.parentNode.parentNode
+        const jobTitle = parenNode.querySelector('.jobTitle').innerText
+        const jobSubTitle = parenNode.querySelector('.jobSubTitle').innerText
+        const jobDetail = parenNode.querySelector('.jobDetail').innerText
+        const status = parenNode.querySelector('.status').innerText
+        const jobNote = parenNode.querySelector('.jobNote').innerText
+        parenNode.querySelector('.status').innerText = 'REJECTED'
+        
+        const cardInfo = {jobTitle, jobSubTitle, jobDetail, status, jobNote}
+        
+        const rejectedExist = rejectedArray.find(item => item.jobTitle == cardInfo.jobTitle)
+        
+        if (!rejectedExist){
+            rejectedArray.push(cardInfo)
+        }
+        interviewArray = interviewArray.filter(item => item.jobTitle != cardInfo.jobTitle)
+        if (currentStatus == "interview-btn") {
+            interviewRender();
+        }
+
+        calculateCount()
     }
 })
 
 function interviewRender(){
     filteredSection.innerHTML = ''
     for(let i of interviewArray){
+        let div = document.createElement('div')
+        div.className = 'job-card'
+        div.innerHTML = `
+        <div>
+            <div class="job-card-gap">
+                <h3 class="jobTitle">${i.jobTitle}</h3>
+                <p class="grey jobSubTitle">${i.jobSubTitle}</p>
+            </div>
+            <p class="job-card-gap grey jobDetail">${i.jobDetail}</p>
+            <p class="job-card-gap status">${i.status}</p>
+            <p class="job-card-gap jobNote">${i.jobNote}</p>
+            <div class="job-card-gap job-card-button">
+                <button class="job-btn-1">INTERVIEW</button>
+                <button class="job-btn-2">REJECTED</button>
+            </div>
+        </div>
+        <div>
+            <button class="job-btn-3"><i class="fa-regular fa-trash-can"></i></button>
+        </div>`
+        filteredSection.appendChild(div)
+    }
+}
+function rejectedRender(){
+    filteredSection.innerHTML = ''
+    for(let i of rejectedArray){
         let div = document.createElement('div')
         div.className = 'job-card'
         div.innerHTML = `
